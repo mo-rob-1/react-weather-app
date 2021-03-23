111f60e08565922cb3b3baf3064db3399d9405e8
Mohammed Rob <mohammed_rob@hotmail.co.uk>
2021-03-23 16:53:05 +0000

    add files and assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const api = {
+  key: process.env.REACT_APP_API_KEY,
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <main>
+        <div className="search-box-container">
+          <input 
+            type="text" 
+            className="search-input" 
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)} 
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-container">
+          <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+
+          <div className="weather-container">
+        <div className="temp">{Math.round(weather.main.temp)}°c</div>
+            <div className="weather">Rain</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
